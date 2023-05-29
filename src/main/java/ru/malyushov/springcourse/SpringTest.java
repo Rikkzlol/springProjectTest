7b8795a92f6259e1fff0ade6edc234841103ad63
Rikkzlol <89975272+Rikkzlol@users.noreply.github.com>
2023-05-29 11:24:29 +0300

    -save

--- src/main/java/ru/malyushov/springcourse/SpringTest.java
+++ src/main/java/ru/malyushov/springcourse/SpringTest.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringTest {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("appllicationContext.xml");
+                new ClassPathXmlApplicationContext("applicationContext.xml");
 
         TestBean testBean = context.getBean("testBean", TestBean.class);
 
